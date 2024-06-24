2a3e68beedc3c29de06a21b14e3a4d4684b79b17
Robert <robert@hjortsholm.com>
2024-06-24 09:31:38 +0200

    Added missing artefacts

---
+++ dist/_app/immutable/chunks/paths.DPKfYPU2.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1fek458)==null?void 0:s.base)??"/roberthmoller";var e;const a=((e=globalThis.__sveltekit_1fek458)==null?void 0:e.assets)??t;export{a,t as b};
